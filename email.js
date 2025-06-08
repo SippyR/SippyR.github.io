403206f5889da5aa45df4d013fd53c98e222a70d
Ryan Sippy <54917000+SippyR@users.noreply.github.com>
2025-06-08 19:34:20 -0400

    Update email.js

--- email.js
+++ email.js
@@ -1,7 +1,7 @@
 var shown = false;
 function showhideEmail(){
 	if(shown){
-		document.getElementById('email').innerHTML = "Show my email";
+		document.getElementById('email').innerHTML = "";
 		shown = false;
 	}else{
 		var myemail = "<a href='mailto:rdsippy"+ "@" + "gmail.com'>rdsippy" + "@" + "gmail.com</a>";
